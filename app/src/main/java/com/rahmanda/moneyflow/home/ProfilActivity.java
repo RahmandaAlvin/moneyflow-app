2c0d30658075ceb835be1b7a61ad8fda2c8d0d1f
rehaniaputrinuraini <rehania2018putri@gmail.com>
2025-12-02 10:43:26 +0700

    INPUT SELESAII YAAWW

---
+++ app/src/main/java/com/rahmanda/moneyflow/home/ProfilActivity.java
@@ -0,0 +1,4 @@
+package com.rahmanda.moneyflow.home;
+
+public class ProfilActivity {
+}
--- app/src/main/java/com/rahmanda/moneyflow/home/ProfilFragment.kt
+++ app/src/main/java/com/rahmanda/moneyflow/home/ProfilFragment.kt
@@ -1,22 +1,38 @@
 package com.rahmanda.moneyflow.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.rahmanda.moneyflow.LoginActivity
 import com.rahmanda.moneyflow.R
 
-
 class ProfilFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false)
-    }
 
+        // Inflate layout
+        val view = inflater.inflate(R.layout.fragment_profil, container, false)
+
+        // Ambil button logout
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+
+        // Aksi logout
+        btnLogout.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+
+            // Agar tidak bisa kembali ke halaman sebelumnya
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+            startActivity(intent)
+        }
+
+        return view
+    }
 }
